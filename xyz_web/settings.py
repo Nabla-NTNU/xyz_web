ba42bbc40f99f7306ab1dadd2277c2572a19a490
Alexander <alexander.arntzen@gmail.com>
2020-09-23 20:25:43 +0200

    added base and bootsrap

--- xyz_web/settings.py
+++ xyz_web/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "node_modules"),)
 
 
 # TODO: Remvoe in production
