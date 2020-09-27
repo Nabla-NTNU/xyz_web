4a7dd20012a79994900e3c1695be17c8396223e2
Thorvald Ballestad <thorvalb@stud.ntnu.no>
2020-09-27 17:05:13 +0200

    Creates base template and makes changes to where template files are placed

--- xyz_web/settings.py
+++ xyz_web/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'xyz_web.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ os.path.join(BASE_DIR, 'templates') ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
